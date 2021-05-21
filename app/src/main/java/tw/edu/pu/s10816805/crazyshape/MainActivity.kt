7012b96e957898059022fe416b1849475791eda1
CrazyShape <a0917983200@gmail.com>
2021-05-21 15:37:06 +0800

    2021上機考v3_(李瑜玲)

--- app/src/main/java/tw/edu/pu/s10816805/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s10816805/crazyshape/MainActivity.kt
@@ -3,6 +3,7 @@ package tw.edu.pu.s10816805.crazyshape
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
@@ -14,9 +15,13 @@ public final class MyAppGlideModule : AppGlideModule()
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
         val img: ImageView = findViewById(R.id.imgTitle)
         GlideApp.with(this)
             .load(R.drawable.cover)
@@ -27,11 +32,33 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(baseContext, "作者：李瑜玲", Toast.LENGTH_LONG).show()
 
-        imgNext.setOnLongClickListener(object : View.OnLongClickListener {
-            override fun onLongClick(p0: View?): Boolean {
+        imgNext.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?){
+                var X: Int = (0..3).random() //目前顯示第幾張圖
+                var TotalPictures: Int = 4
+                fun ShowPicture() {
+                    when (X) {
+                        0 -> img.setImageResource(R.drawable.circle)
+                        1 -> img.setImageResource(R.drawable.square)
+                        2 -> img.setImageResource(R.drawable.star)
+                        3 -> img.setImageResource(R.drawable.triangle)
+
+                    }
+                }
+                X = X
+                ShowPicture()
+
+
+
+
+
+                /*imgNext.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?): Boolean {
                 intent = Intent(this@MainActivity, GameActivity::class.java)
                 startActivity(intent)
                 return true
+            }
+        })*/
             }
         })
     }
